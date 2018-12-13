b95f16e432c6b0f6439ff1ac2a38bc545303bd57
Brian DiRito <bdirito@gmail.com>
2018-12-13 16:55:10 -0500

    started work on users_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::UsersController < ApplicationController
     def create
         user = User.create(user_params)
-        
+        if user.valid?
+            render json: user
+        else
+            render json: {"error" => user.errors.full_messages}, status: 422
+        end
+    end
+
+    def destroy
+        render json: {"message" => "Account Has Been Deactivated"}
     end
 
     private
