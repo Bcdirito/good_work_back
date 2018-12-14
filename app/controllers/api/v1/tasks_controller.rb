7d18a0a351655bda7918abd0b0a3de3c100bd5ac
Brian DiRito <bdirito@gmail.com>
2018-12-14 11:04:11 -0500

    can create in all controllers

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,20 @@
-class TasksController < ApplicationController
+class Api::V1::TasksController < ApplicationController
+    def index
+        render json: Task.all
+    end
+
+    def create
+        task = Task.create(task_params)
+        if task.valid?
+            render json: task
+        else
+            render json: {"error" => task.errors.full_messages}, status: 422
+        end
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:title, :content, :list_id)
+    end
 end
