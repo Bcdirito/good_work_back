14869cd08e9011734da696ea4f1c644c43092981
Brian DiRito <bdirito@gmail.com>
2018-12-14 10:59:58 -0500

    can create lists

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,2 +1,20 @@
-class ListsController < ApplicationController
+class Api::V1::ListsController < ApplicationController
+    def index
+        render json: Index.all
+    end
+
+    def create
+        list = List.create(list_params)
+        if list.valid?
+            render json: list
+        else
+            render json: {"error" => list.errors.full_messages}, status: 422
+        end
+    end
+
+    private
+
+    def list_params
+        params.require(:list).permit(:name, :goal_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       # resources :partners, only: [:get, :create, :update, :show]
-      resources :tasks, only: [:create, :update, :destroy]
-      resources :lists, only: [:create, :update, :destroy, :show]
-      resources :goals, only: [:create, :update, :destroy, :show]
+      resources :tasks, only: [:index, :create, :update, :destroy]
+      resources :lists, only: [:index, :create, :update, :destroy, :show]
+      resources :goals, only: [:index, :create, :update, :destroy, :show]
       resources :users, only: [:create, :update, :show, :destroy]
     end
   end
