04e22bfab0de4cd8774a585e8dfb0be845df8b1f
Brian DiRito <bdirito@gmail.com>
2018-12-14 10:50:22 -0500

    can create goals

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -2,4 +2,19 @@ class Api::V1::GoalsController < ApplicationController
     def index
         render json: Goal.all
     end
+
+    def create
+        goal = Goal.create(goal_params)
+        if goal.valid?
+            render json: goal
+        else
+            render json: {"error" => goal.errors.full_messages}, status: 422
+        end
+    end
+
+    private
+
+    def goal_params
+        params.require(:goal).permit(:name, :user_id)
+    end
 end
