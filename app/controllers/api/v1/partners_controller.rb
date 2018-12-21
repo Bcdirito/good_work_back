9caa23a2b909be1cadf3f4743cf92a4c74efe3d8
Brian DiRito <bdirito@gmail.com>
2018-12-20 23:04:34 -0500

    implementing partners

--- app/controllers/api/v1/partners_controller.rb
+++ app/controllers/api/v1/partners_controller.rb
@@ -1,2 +1,40 @@
 class Api::V1::PartnersController < ApplicationController
+    skip_before_action :authorized, only: [:index]
+
+    def index
+        render json: Partner.all
+    end
+
+    def create
+        partner = Partner.create(partner_params)
+        if partner.valid?
+            render json: partner
+        else
+            render json: {"error" => partner.errors.full_messages}, status: 422
+        end
+    end
+
+    def update
+        partner = Partner.update(params[:id], partner_params)
+        if partner.valid?
+            render json: partner
+        else
+            render json: {"error" => partner.errors.full_messages}
+        end
+    end
+
+    def destroy
+        partner = Partner.find(params[:id])
+        if partner.destroy
+            render json: {"message" => "partner Has Been Deleted"}
+        else
+            render json: {"error" => partner.errors.full_messages}, status: 409
+        end
+    end
+
+    private
+
+    def partner_params
+        params.require(:partner).permit(:name, :email, :user_id)
+    end
 end
