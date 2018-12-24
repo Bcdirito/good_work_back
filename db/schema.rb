56bb725e0e4ad53b7c25ca2d432353b23a7de60c
Brian DiRito <bdirito@gmail.com>
2018-12-24 16:38:51 -0500

    added names to table. Reset DB

--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2018_12_20_191804) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
