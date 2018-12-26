e26d59926c522a9067034f1312b342e0cb947fc7
Brian DiRito <bdirito@gmail.com>
2018-12-26 16:41:55 -0500

    fixed user signup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -42,6 +42,6 @@ class Api::V1::UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(:username, :password, :email)
+        params.permit(:username, :password, :email, :name)
     end
 end
