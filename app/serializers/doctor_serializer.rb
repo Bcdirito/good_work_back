4925f65e5d1ecd215b9d187f897e848a8b3571e9
Brian DiRito <bdirito@gmail.com>
2019-01-23 17:53:33 -0500

    added image_url to table

--- app/serializers/doctor_serializer.rb
+++ app/serializers/doctor_serializer.rb
@@ -1,5 +1,5 @@
 class DoctorSerializer < ActiveModel::Serializer
-  attributes :id, :name, :bio
+  attributes :id, :name, :bio, :image_url
   has_many :practices
   has_many :users, through: :doctor_users
 end
