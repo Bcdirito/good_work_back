933762df9f16ba398b7c3bbba5af189076d69d5b
Brian DiRito <bdirito91@gmail.com>
2019-02-10 21:20:56 -0500

    more heroku action mailer work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,13 +80,14 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.default_url_options = { :host => "https://good-work-backend.herokuapp.com/"}
   config.action_mailer.delivery_method = :smtp
    config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
       port: 587,
-      domain: "https://good-work-backend.herokuapp.com/",
-      user_name: "goodworkmailer@gmail.com",
-      password: "B1anCat3",
+      domain: Rails.application.secrets.domain,
+      user_name: Rails.application.secrets.email_user_name,
+      password: Rails.application.secrets.email_password,
       authentication: 'plain',
       enable_starttls_auto: true
     }
