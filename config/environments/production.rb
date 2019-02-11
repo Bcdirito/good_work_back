0237c23b44f925bfbb2a0ccf272705103086868c
Brian DiRito <bdirito91@gmail.com>
2019-02-10 19:51:44 -0500

    revising for heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,15 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+     address: 'smtp.mail.com',
+     port: 587,
+     domain: Rails.application.secrets.domain_name,
+     user_name: Rails.application.secrets.email_user_name,
+     password: Rails.application.secrets.email_password,
+     authentication: 'plain',
+     enable_starttls_auto: true }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
