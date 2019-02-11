53181c75456340c652fe6a0376dd33c249f4391f
Brian DiRito <bdirito91@gmail.com>
2019-02-10 19:57:48 -0500

    action mailer refactor for production heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,14 +32,14 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => Rails.application.secrets.email_user_name,
-    :password => Rails.application.secrets.email_password,
-    :authentication => "plain",
-    :enable_starttls_auto => true
-  }
+    config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :user_name => Rails.application.secrets.email_user_name,
+      :password => Rails.application.secrets.email_password,
+      :authentication => "plain",
+      :enable_starttls_auto => true
+    }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
