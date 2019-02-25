246f5dbd2c5ae141b1d387a94f4476e302ab951c
Brian DiRito <bdirito91@gmail.com>
2019-02-25 16:37:23 -0500

    fixing heroku production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,8 @@ Rails.application.configure do
    config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
       port: 587,
-      domain: ENV["GMAIL_DOMAIN"]
-      user_name: ENV["GMAIL_USERNAME"]
+      domain: ENV["GMAIL_DOMAIN"],
+      user_name: ENV["GMAIL_USERNAME"],
       password: ENV["GMAIL_PASSWORD"],
       authentication: 'plain',
       enable_starttls_auto: true
